7791b10f011a9bcbb49ecab21596114f3f636201
lee-orr <lee-orr@users.noreply.github.com>
2023-01-22 23:42:48 -0500

    transform camera, delete placed things

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::{
-    math::swizzles::*, prelude::*, render::camera::RenderTarget, sprite::MaterialMesh2dBundle,
+    math::swizzles::*,
+    prelude::*,
+    render::camera::{RenderTarget, ScalingMode},
+    sprite::MaterialMesh2dBundle,
 };
 
 fn main() {
@@ -21,7 +24,8 @@ fn main() {
         .add_system(calculate_gravity)
         .add_system(move_velocity)
         .add_system(check_goal)
-        .add_system(gravity_spawner);
+        .add_system(gravity_spawner)
+        .add_system(position_main_camera);
 
     app.run();
 }
@@ -31,7 +35,13 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((Camera2dBundle {
+        projection: OrthographicProjection {
+            scaling_mode: ScalingMode::FixedHorizontal(BUFFER),
+            ..default()
+        },
+        ..default()
+    }, MainCamera, ElasticCentering(1., 0.)));
 
     commands.spawn((
         MaterialMesh2dBundle {
@@ -40,7 +50,7 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(0., -100., 0.)),
             ..default()
         },
-        GravitationalBody(10000.),
+        GravitationalBody(10000., 50.),
         Velocity::Static,
     ));
 
@@ -61,14 +71,14 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(-500., 0., 0.)),
             ..default()
         },
-        GravitationalBody(1.),
+        GravitationalBody(1., 10.),
         Player,
         Velocity::Value(Vec2::X * 50.),
     ));
 }
 
 #[derive(Component)]
-pub struct GravitationalBody(f32);
+pub struct GravitationalBody(f32, f32);
 
 #[derive(Component)]
 pub enum Velocity {
@@ -81,6 +91,15 @@ pub struct Player;
 
 #[derive(Component)]
 pub struct Goal(f32);
+
+#[derive(Component)]
+pub struct MainCamera;
+
+#[derive(Component)]
+pub struct ElasticCentering(f32, f32);
+
+#[derive(Component)]
+pub struct Deletable;
 
 const G: f32 = 30.;
 
@@ -99,7 +118,7 @@ fn calculate_gravity(
                 let r = (t_2.translation - transform.translation).xy();
                 info!("Adjusting velocity {new_v:?}");
                 let d_sq = r.length_squared();
-                if d_sq != 0. {
+                if d_sq > 30. {
                     new_v +=
                         (G * time.delta_seconds() * gravity.0 * g_2.0 * r.normalize()) / (d_sq);
                 }
@@ -144,6 +163,7 @@ fn gravity_spawner(
 
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    existing_gravity: Query<(Entity, &GlobalTransform, &GravitationalBody), With<Deletable>>
 ) {
     if !buttons.just_pressed(MouseButton::Left) {
         return;
@@ -175,6 +195,17 @@ fn gravity_spawner(
         // reduce it to a 2D value
         let world_pos: Vec2 = world_pos.truncate();
 
+        let mut deleted = false;
+
+        for (entity, transform, gravity) in existing_gravity.iter() {
+            if transform.translation().xy().distance(world_pos) < gravity.1 {
+                deleted = true;
+                commands.entity(entity).despawn_recursive();
+            }
+        } 
+        
+        if !deleted {
+
         commands.spawn((
             MaterialMesh2dBundle {
                 mesh: meshes.add(shape::Circle::new(50.).into()).into(),
@@ -182,8 +213,67 @@ fn gravity_spawner(
                 transform: Transform::from_translation(Vec3::new(world_pos.x, world_pos.y, 0.)),
                 ..default()
             },
-            GravitationalBody(10000.),
+            GravitationalBody(10000., 50.),
             Velocity::Static,
+            Deletable
         ));
+    }
+    }
+}
+
+const BUFFER: f32 = 1500.0;
+const EDGE: f32 = 300.0;
+
+fn position_main_camera(
+    mut camera: Query<
+        (&mut Transform, &mut OrthographicProjection, &mut ElasticCentering),
+        (With<MainCamera>, Without<Player>),
+    >,
+    players: Query<&Transform, With<Player>>,
+    time: Res<Time>
+) {
+    let mut player_bounds = None;
+    for player in players.iter() {
+        if let Some((min, max)) = player_bounds {
+            player_bounds = Some((
+                player.translation.xy().min(min),
+                player.translation.xy().max(max),
+            ));
+        } else {
+            player_bounds = Some((player.translation.xy(), player.translation.xy()));
+        }
+    }
+
+    for (mut transform, mut projection, mut centering) in camera.iter_mut() {
+        let camera_center = transform.translation.xy();
+        let camera_scale_w = projection.right;
+        let camera_scale_h = projection.top;
+
+        let (gap, pos) = if let Some((min, max)) = player_bounds {
+            let gap = (max - min) / 2.;
+            (gap, gap + min)
+        } else {
+            (Vec2::ZERO, Vec2::ZERO)
+        };
+
+        let max_component = gap.max_element();
+        let horizontal = BUFFER + max_component;
+
+        let horizontal_edge = (camera_center.x - camera_scale_w + EDGE) > pos.x - gap.x / 2. || (camera_center.x + camera_scale_w - EDGE) < pos.x + gap.x/2.;
+        let vertical_edge = (camera_center.y - camera_scale_h + EDGE) > pos.y - gap.y / 2. || (camera_center.y + camera_scale_h - EDGE) < pos.y + gap.y/2.;
+
+        if horizontal_edge || vertical_edge {
+            centering.1 = centering.0;
+        } else {
+            centering.1 -= time.delta_seconds();
+        }
+
+        if centering.1 > 0. {
+            let target_dist = (pos - camera_center) * 2. ;
+            let pos = target_dist * time.delta_seconds() + camera_center;
+            transform.translation = Vec3::new(pos.x, pos.y, 0.);
+        }
+
+        projection.scaling_mode = ScalingMode::FixedHorizontal(horizontal);
     }
 }
